8c2cc894e2d296839b2a6a43c1f3584c6cfe5f42
wlstyql <apxkfrj@naver.com>
2019-04-19 00:44:01 +0900

    DL_from_scratch

---
+++ DL_from_scratch(Book)/common/functions.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def identity_function(x):
+    return x
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def softmax(x):
+    c = np.max(x)
+    exp_x = np.exp(x - c) # overflow prevention
+    sum_exp_x = np.sum(exp_x)
+    return exp_x / sum_exp_x
