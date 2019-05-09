76ccf9cb37f0f39ac1d8dd843d1d00e603d7a849
wlstyql <apxkfrj@naver.com>
2019-05-09 10:55:25 +0900

    DL_from_scratch

--- DL_from_scratch(Book)/common/functions.py
+++ DL_from_scratch(Book)/common/functions.py
@@ -17,3 +17,10 @@ def softmax(x):
 
 def relu(x):
     return np.maximum(0, x)
+
+def mse(y, t):
+    return 0.5 * np.sum((y-t)**2)
+
+def cross_entropy_error(y, t):
+    delta = 1e-7
+    return -np.sum(t * np.log(y + delta))
