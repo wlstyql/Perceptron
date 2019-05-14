e388c461686ca130e2169e8bd461172085f979c2
wlstyql <apxkfrj@naver.com>
2019-05-14 16:53:13 +0900

    DL_from_scratch

--- DL_from_scratch(Book)/common/loss_functions.py
+++ DL_from_scratch(Book)/common/loss_functions.py
@@ -6,3 +6,11 @@ def mean_squared_error(y, t):
 def cross_entropy_error(y, t):
     delta = 1e-7
     return -np.sum(t * np.log(y + delta))
+
+def batch_cross_entropy_error(y, t):
+    delta = 1e-7
+    if y.ndim == 1:
+        batch_size = 1
+    else:
+        batch_size = y.shape[0]    
+    return -np.sum(t * np.log(y + delta)) / batch_size   
