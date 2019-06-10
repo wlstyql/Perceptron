4636d5365b6b14325a641466ad31767c861d09dc
wlstyql <apxkfrj@naver.com>
2019-06-10 14:21:29 +0900

    DL_from_scratch

--- DL_from_scratch(Book)/common/functions.py
+++ DL_from_scratch(Book)/common/functions.py
@@ -22,20 +22,32 @@ def numerical_diff(f, x):
     h = 1e-4
     return (f(x+h) - f(x-h)) / (2*h)
 
-def numerical_gradient(f, x):
-    h = 1e-4
-    grad = np.zeros_like(x)
+def _numerical_gradient_no_batch(f, x):
+    h = 1e-4 # 0.0001
+    grad = np.zeros_like(x) # x와 형상이 같은 배열을 생성
     
     for idx in range(x.size):
         tmp_val = x[idx]
-        x[idx] = tmp_val + h
-        fxh1 = f(x)  # f(x+h)
         
-        x[idx] = tmp_val - h
-        fxh2 = f(x)  # f(x-h)
+        x[idx] = tmp_val + h  # f(x+h) 계산
+        fxh1 = f(x)
+        
+        x[idx] = tmp_val - h  # f(x-h) 계산
+        fxh2 = f(x) 
         
         grad[idx] = (fxh1 - fxh2) / (2*h)
-        x[idx] = tmp_val
+        x[idx] = tmp_val # 값 복원
         
     return grad
+
+
+def numerical_gradient(f, X):
+    if X.ndim == 1:
+        return _numerical_gradient_no_batch(f, X)
+    else:
+        grad = np.zeros_like(X)
         
+        for idx, x in enumerate(X):
+            grad[idx] = _numerical_gradient_no_batch(f, x)
+        
+        return grad
