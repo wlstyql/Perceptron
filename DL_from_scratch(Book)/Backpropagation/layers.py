61cc3d99a64acd1718bb7391502970787f33231a
wlstyql <apxkfrj@naver.com>
2019-06-24 15:24:11 +0900

    DL_from_scratch

--- DL_from_scratch(Book)/Backpropagation/layers.py
+++ DL_from_scratch(Book)/Backpropagation/layers.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class MulLayer:
     def __init__(self):
         self.x = None
@@ -53,4 +55,23 @@ class Sigmoid:
     
     def backward(self, dout):
         dx = dout * self.out * (1.0 - self.out)
+        return dx
+    
+class Affine:
+    def __init__(self, W, b):
+        self.W = W
+        self.b = b
+        self.x = None
+        self.dW = None
+        self.db = None
+        
+    def forward(self, x):
+        self.x = x
+        out = np.dot(x, self.W) + self.b
+        return out
+    
+    def backward(self, dout):
+        dx = np.dot(dout, self.W.T)
+        self.dW = np.dot(self.x.T, dout)
+        self.db = np.sum(dout, axis=0)
         return dx
